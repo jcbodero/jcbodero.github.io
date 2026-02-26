c7d3b28a7f00cb2ff6d205f67098ba71a41cfdeb
Julio Bodero <j.boderoc@gmail.com>
2026-02-26 13:42:10 +0000

    npm run build
    > nextn@0.1.0 build
    > NODE_ENV=production next build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,12 @@
-
 import { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static';
+export const revalidate = false;
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://jcbodero.com', // Reemplazar con el dominio real si es diferente
+      url: 'https://jcbodero.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
