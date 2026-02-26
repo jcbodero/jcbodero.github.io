3840f424839ce5db875bfeab14a2994cbd70b5ca
Julio Bodero <j.boderoc@gmail.com>
2026-02-26 19:24:53 +0000

    Internal Server Error no carga la pagina

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: '',
+  basePath: '/jcbodero.github.io',
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
