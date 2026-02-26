6da024eed4e6fe55bdb5e04fa4d93ab96c61c579
Julio Bodero <j.boderoc@gmail.com>
2026-02-26 14:14:05 +0000

    Tienes que decirle a Next.js dos verdades:
    “Mi sitio NO vive en /”
    
    “Vi

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/jcbodero.github.io',
   typescript: {
     ignoreBuildErrors: true,
   },
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export const revalidate = false;
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://jcbodero.com',
+      url: 'https://jcbodero.com/jcbodero.github.io',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
